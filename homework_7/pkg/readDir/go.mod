30d1bf0d34728f2e5ad4c1ba5001e4a7fa660e1e
andreiDmitriev2022 <107244116+andreiDmitriev2022@users.noreply.github.com>
2024-02-13 23:52:43 +0500

    создание модуля readDir

---
+++ homework_7/pkg/readDir/go.mod
@@ -0,0 +1,3 @@
+module github.com/andrei1998Front/go_course/homework_7/pkg/readDir
+
+go 1.21.4
---
+++ homework_7/pkg/readDir/readDir.go
@@ -0,0 +1,9 @@
+package readdir
+
+import "fmt"
+
+func ReadDir(dir string) (map[string]string, error) {
+	fmt.Println("Hello from readString!")
+
+	return nil, nil
+}
